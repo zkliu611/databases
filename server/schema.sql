ca5e5ee5c195f6718abc125d466a31c87f895834
zkliu611 <zkliu61@gmail.com>
2018-12-05 17:22:43 -0800

    implement schema

--- server/schema.sql
+++ server/schema.sql
@@ -3,7 +3,26 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id int AUTO_INCREMENT PRIMARY KEY,
+  user varchar(255),
+  room varchar(255), 
+  messageText text
+  FOREIGN KEY(user) references users(id),
+  FOREIGN KEY(room) references rooms(id)
+  /* this is our main table that contains all the messages data.*/
+);
+
+CREATE TABLE users (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  name varchar(255)
+  /* this is our user data table.*/
+);
+
+CREATE TABLE rooms (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  name varchar(255)
+
+  /* this is our room data table.*/
 );
 
 /* Create other tables and define schemas for them here! */
